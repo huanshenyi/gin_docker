60d206755fd12f7ce77c8348dd5f5a4439313929
den <den@dennoMacBook-Pro.local>
2020-11-03 21:47:02 +0900

    開発環境初期化

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello world11d",
+		})
+	})
+	router.GET("/hi", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hi",
+		})
+	})
+	router.Run(":8080")
+}
