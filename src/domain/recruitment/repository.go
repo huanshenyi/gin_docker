b6b46c4a6ea243fd80de9829ec4d9be35bc387fc
huanshenyi <txy1226052@gmail.com>
2022-08-20 08:24:55 +0900

    commit追加

--- src/domain/recruitment/repository.go
+++ src/domain/recruitment/repository.go
@@ -2,9 +2,10 @@ package recruitment
 
 import "gin_docker/src/domain"
 
+// 募集テーブルに対する操作
 type Repository interface {
 	ListRecruitmentForUserID(tx domain.Tx, userID int) (Recruitments, error)
-	CreateRecruitment(tx domain.Tx, input domain.Recruitment) error
+	CreateRecruitment(tx domain.Tx, input domain.Recruitment) error //新規募集
 }
 
 type Recruitments struct {
